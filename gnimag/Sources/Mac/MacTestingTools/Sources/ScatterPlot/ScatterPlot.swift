550c0eb2e34095a9930096ba1cca89d63cd49a86
knothed <dknothe314@me.com>
2019-08-07 19:23:45 +0200

    Implement ScatterPlot using Charts

--- gnimag/Sources/Mac/MacTestingTools/Sources/ScatterPlot/ScatterPlot.swift
+++ gnimag/Sources/Mac/MacTestingTools/Sources/ScatterPlot/ScatterPlot.swift
@@ -3,11 +3,37 @@
 //  Copyright © 2019 Piknotech. All rights reserved.
 //
 
+import Cocoa
 import Charts
 
 public enum ScatterPlot {
     /// Create a scatter plot with the given data and save it to a file.
-    public static func create(withXValues xValues: [Double], yValues: [Double], saveToFile: String) {
-        // ...
+    /// The scatter plot is black and is drawn on a white background.
+    public static func create(withXValues xValues: [Double], yValues: [Double], scatterCircleSize: CGFloat, outputImageSize: CGSize, saveTo file: String) {
+        // Map values to ChartDataEntries
+        let entries = zip(xValues, yValues).map { x, y in
+            ChartDataEntry(x: x, y: y)
+        }
+
+        // Create DataSet and view
+        let dataSet = ScatterChartDataSet(entries: entries, label: "DataSet")
+        dataSet.setScatterShape(.circle)
+        dataSet.colors = [.black]
+        dataSet.scatterShapeSize = scatterCircleSize
+        let data = ScatterChartData(dataSet: dataSet)
+
+        let view = ScatterChartView(frame: CGRect(origin: .zero, size: outputImageSize))
+        view.data = data
+
+        // Save view to file
+        let image = NSImage(size: view.bounds.size)
+        image.lockFocusFlipped(true)
+        NSColor.white.drawSwatch(in: view.bounds) // White background
+        view.draw(view.bounds)
+        image.unlockFocus()
+
+        let rep = NSBitmapImageRep(data: image.tiffRepresentation!)!
+        let viewData = rep.representation(using: .png, properties: [:])!
+        NSData(data: viewData).write(toFile: file, atomically: true)
     }
 }
