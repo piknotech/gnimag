a3a2a770de28b9b3141fd8519432b0bcb2e96d4c
David Knothe <dknothe314@me.com>
2021-03-08 17:56:53 +0100

    Add module header

---
+++ gnimag/Modules/Games/ThreePoints/SupportingFiles/ThreePoints.h
@@ -0,0 +1,16 @@
+//
+//  Created by David Knothe on 07.04.20.
+//  Copyright © 2019 - 2021 Piknotech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ThreePoints.
+FOUNDATION_EXPORT double ThreePointsVersionNumber;
+
+//! Project version string for ThreePoints.
+FOUNDATION_EXPORT const unsigned char ThreePointsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreePoints/PublicHeader.h>
+
+
