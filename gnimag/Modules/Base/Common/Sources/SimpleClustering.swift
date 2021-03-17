piknotech/gnimag

commit 807d483522d4e50d69c8ea7ec0e34068e7fe75ac

Create `Array.sorted(by key: (Element) -> A)`

gnimag/Modules/Base/Common/Sources/SimpleClustering.swift
@@ -104,7 +104,7 @@ public enum SimpleClustering {
         }
 
         // Sort clusters by size and return result
-        clusters.sort { $0.objects.count > $1.objects.count }
+        clusters.sort(by: \.objects.count)
         return Result(clusters: clusters)
     }
 }

gnimag/Modules/Base/Common/Sources/Extensions/Array+Sort.swift
@@ -1,0 +1,30 @@
+//
+//  Created by David Knothe on 15.03.21.
+//  Copyright © 2019 - 2021 Piknotech. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    @_transparent
+    public mutating func sort<A>(by key: (Element) -> A, _ smaller: (A, A) -> Bool) {
+        sort { smaller(key($0), key($1)) }
+    }
+
+    @_transparent
+    public mutating func sort<A: Comparable>(by key: (Element) -> A) {
+        sort(by: key, <)
+    }
+
+    @_transparent
+    public func sorted<A>(by key: (Element) -> A, _ smaller: (A, A) -> Bool) -> [Element] {
+        var result = self
+        result.sort(by: key, smaller)
+        return result
+    }
+
+    @_transparent
+    public func sorted<A: Comparable>(by key: (Element) -> A) -> [Element] {
+        sorted(by: key, <)
+    }
+}

gnimag/Modules/Base/GameKit/Sources/Tapping/Infrastructure/Models/TapSequence.swift
@@ -45,7 +45,7 @@ public struct RelativeTapSequence {
         if isAlreadySorted {
             self.taps = taps
         } else {
-            self.taps = taps.sorted { $0.relativeTime < $1.relativeTime }
+            self.taps = taps.sorted(by: \.relativeTime)
         }
 
         self.unlockDuration = unlockDuration
@@ -59,7 +59,7 @@ public struct RelativeTapSequence {
 
 extension RelativeTapSequence: CustomStringConvertible {
     public var description: String {
-        let sortedTaps = taps.sorted { $0.relativeTime < $1.relativeTime }
+        let sortedTaps = taps.sorted(by: \.relativeTime)
         return "RelativeTapSequence(taps: \(sortedTaps), unlockDuration: \(String(describing: unlockDuration)))"
     }
 }

gnimag/Modules/Base/GameKit/Sources/Tapping/Infrastructure/TapDelayTracker.swift
@@ -38,7 +38,7 @@ public final class TapDelayTracker {
     /// Call when a tap has just been performed at the given time.
     public func tapPerformed(_ tap: PerformedTap) {
         performedTaps.append(tap)
-        performedTaps.sort { $0.scheduledFor < $1.scheduledFor }
+        performedTaps.sort(by: \.scheduledFor)
     }
 
     /// Call when a tap has just been detected at the given time.

gnimag/Modules/Base/ImageAnalysisKit/Sources/Algorithms/BitmapOCR/ConnectedComponents.swift
@@ -44,10 +44,7 @@ internal enum ConnectedComponents {
         combine(components: &components, using: combineComponents)
 
         // Sort left-to-right
-        components.sort { a, b in
-            a.region.xRange.center < b.region.xRange.center
-        }
-
+        components.sort(by: \.region.xRange.center)
         return components
     }
 

gnimag/Modules/Debug/TestingTools/Sources/ScatterPlot/ScatterPlot.swift
@@ -28,7 +28,7 @@ public final class ScatterPlot {
         yRange: SimpleRange<Double>? = nil,
         rangeAugmentation: Double = 5%
     ) {
-        let dataPoints = dataPoints.sorted { $0.x < $1.x }
+        let dataPoints = dataPoints.sorted(by: \.x)
 
         // Map sorted values to ChartDataEntries
         let entries = dataPoints.map { ChartDataEntry(x: $0.x, y: $0.y) }

gnimag/Modules/Games/FlowFree/Sources/1 ImageAnalysis/ImageAnalyzer.swift
@@ -58,7 +58,7 @@ class ImageAnalyzer {
         }
 
         let result = SimpleClustering.from(elements, maxDistance: 0.1)
-        let clusters = result.clusters.sorted { $0.size < $1.size }
+        let clusters = result.clusters.sorted(by: \.size)
 
         // Check cluster validity: all clusters (except the background color) must have 2 elements
         if (clusters.dropLast().any { $0.size != 2 }) {

gnimag/Modules/Games/MrFlap/Sources/3 TapPrediction/Models/PredictionFrame/PredictionFrame.swift
@@ -35,8 +35,6 @@ extension PredictionFrame {
         gmc.model.bars.compactMap { tracker in
             guard let bar = BarProperties(bar: tracker, with: gmc.model.player, playfield: playfield, currentTime: currentTime, gmc: gmc) else { return nil }
             return PlayerBarInteraction(player: player, bar: bar, playfield: playfield, currentTime: currentTime, barTracker: tracker)
-        }.sorted {
-            $0.timeUntilLeaving < $1.timeUntilLeaving
-        }
+        }.sorted(by: \.timeUntilLeaving)
     }
 }

commit e7fdd2741670a6c888d2f1d9cb40aaf2f57d4601

Fix sorting issue introduced by 807d483

gnimag/Modules/Base/Common/Sources/SimpleClustering.swift
@@ -104,7 +104,7 @@ public enum SimpleClustering {
         }
 
         // Sort clusters by size and return result
-        clusters.sort(by: \.objects.count)
+        clusters.sort(by: \.objects.count, >)
         return Result(clusters: clusters)
     }
 }