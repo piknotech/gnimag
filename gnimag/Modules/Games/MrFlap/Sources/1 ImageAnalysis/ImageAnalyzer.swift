3ce443fd51ae535bf9cabb80e02b2fe912467b56
Frederick Pietschmann <19194800+fredpi@users.noreply.github.com>
2021-05-10 23:52:41 +0200

    Use new algorithm to reliably find theme color for MrFlap

--- gnimag/Modules/Games/MrFlap/Sources/1 ImageAnalysis/ImageAnalyzer.swift
+++ gnimag/Modules/Games/MrFlap/Sources/1 ImageAnalysis/ImageAnalyzer.swift
@@ -80,9 +80,13 @@ class ImageAnalyzer {
 
     /// Find the coloring of the game.
     private func findColoring(in image: Image) -> Coloring? {
-        // Step 1: use static pixel to find the main (theme) color
-        let topLeft = Pixel(10, image.bounds.height - 100)
-        let theme = image.color(at: topLeft)
+        // Step 1: find the main (theme) color by walking down from the top
+        var theme: Color = .black
+        for pixel in ((0..<image.bounds.height/50).map { Pixel(10, image.bounds.height - $0 * 50) }) {
+            theme = image.color(at: pixel)
+            let channelSum = theme.blue + theme.red + theme.green
+            if channelSum > 0.3 && channelSum < 2.7 { break } // Neither black nor white => color is found
+        }
 
         // Step 2: consider 21 pixels and determine their most frequent color to find the secondary color
         let circle = Circle(center: image.bounds.center.CGPoint, radius: CGFloat(image.width) / 4)
